fa586a4ae0031328e56f2715e9fcffa26c8273df
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-21 06:13:02 -0400

    Set up a basic server

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
 const app = express();
+
+// Listening for requests
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Server has successfully started.");
+})
