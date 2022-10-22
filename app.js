956bd9337793a00e8794797ac6782268f53dba5a
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-22 08:01:12 -0400

    Connected app to db and created db schema

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Starting the express app
 const app = express();
+
+// Connecting to database
+mongoose.connect("mongodb+srv://AryanK1511:<password>@bookworm.qvd4tsp.mongodb.net/?retryWrites=true&w=majority/BooksDB", {useNewUrlParser: true});
+
+// Creating a schema for the database
+const bookSchema = new mongoose.Schema ({
+    bookName: {
+        type: String,
+        required: [true, "No name has been provided for the book."]
+    },
+    bookAuthor: String,
+    DateAdded: String
+});
 
 // Listening for requests
 let port = process.env.PORT;
