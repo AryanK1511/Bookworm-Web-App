82687a1090ec45254cd1fd5c61b8846028259099
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-27 09:27:24 -0400

    Added local login and register strategies using passport

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Global variables
+var readingListLoginRequest = false;
+
 // Using passport sessions
 app.use(session({
     secret: String(process.env.SESSION_SECRET),
@@ -25,11 +28,12 @@ app.use(session({
     saveUninitialized: false
 }))
 
+// Initializing passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Connecting to database
-mongoose.connect("mongodb+srv://AryanK1511:" + process.env.MONGO_ATLAS_PASSKEY + "@bookworm.qvd4tsp.mongodb.net/?retryWrites=true&w=majority/BooksDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://AryanK1511:" + process.env.MONGO_ATLAS_PASSKEY + "@bookworm.qvd4tsp.mongodb.net/BookWorm", {useNewUrlParser: true});
 
 // Creating a schema for the books database
 const bookSchema = new mongoose.Schema ({
@@ -71,7 +75,10 @@ passport.deserializeUser(function(id, done) {
 
 // ========== HOME ROUTE =========
 app.get("/", (req, res) => {
-    res.render("Index");
+    readingListLoginRequest = false;
+    res.render("Index", {
+        user: req.user
+    });
 })
 
 // ========== LOGIN ROUTE ========
@@ -91,8 +98,26 @@ app.get("/search-results", (req, res) => {
 
 // ========== READING LIST ROUTE ========== 
 app.get("/reading-list", (req, res) => {
-    res.render("ReadingList");
+    if (req.isAuthenticated()) {
+        res.render("ReadingList");
+    }
+    else {
+        readingListLoginRequest = true;
+        res.render("Login");
+    }
 })
+
+// ====================== LOGOUT ROUTE ===================
+app.get("/logout", function(req, res) {
+    req.logout(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/");
+        }
+    });
+});
 
 // ========== POSTING USING THE SEARCH FORM ==========
 app.post("/search-results", (req, res) => {
@@ -111,41 +136,48 @@ app.post("/search-results", (req, res) => {
         })
 })
 
-// // =========== Posting to the register route ==========
-// app.post("/register", (req, res) => {
-//     // Registering the user to our application and storing their data in our database
-//     User.register({username: req.body.username}, req.body.password, function(err) {
-//         if (err) {
-//             console.log(err);
-//             res.redirect("/register");
-//         }
-//         else {
-//             passport.authenticate("local")(req, res, function() {
-//                 res.redirect("/");
-//             })
-//         }
-//     });
-// })
+// =========== Posting to the register route ==========
+app.post("/register", (req, res) => {
+    // Registering the user to our application and storing their data in our database
+    User.register({username: req.body.username}, req.body.password, function(err) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/");
+            })
+        }
+    });
+})
 
 // // =========== Posting to the login route ==========
-// app.post("/login", (req, res) => {
-//     // Logging in the user by checking if the credentials match using passport
-//     const user = new User({
-//         username: req.body.username,
-//         password: req.body.password
-//     })
+app.post("/login", (req, res) => {
+    // Logging in the user by checking if the credentials match using passport
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
 
-//     req.login(user, function(err) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             passport.authenticate("local")(req, res, function() {
-//                 res.redirect("/secrets");
-//             })
-//         }
-//     });
-// })
+    // Using passport to login the user and authenticate them
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function() {
+                // Checking whether the login was requested manually or to access the reading list
+                if (readingListLoginRequest) {
+                    res.redirect("/reading-list");
+                }
+                else {
+                    res.redirect("/");
+                }
+            })
+        }
+    });
+})
 
 // Listening for requests
 let port = process.env.PORT;
