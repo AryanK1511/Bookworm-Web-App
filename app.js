08cf6cdc010c9999f29283d42584cc54fc22e94b
Aryan Khurana <aryankhurana1511@gmail.com>
2023-03-07 13:38:47 -0500

    Fixed bugs

--- app.js
+++ app.js
@@ -48,8 +48,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Connecting to database
-
-mongoose.connect("mongodb+srv://AryanK1511:" + process.env.MONGO_ATLAS_PASSKEY + "@bookworm.qvd4tsp.mongodb.net/BookWorm", {useNewUrlParser: true});
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://AryanK1511:" + process.env.MONGO_ATLAS_PASSKEY + "@bookworm.qvd4tsp.mongodb.net/BookWorm", {useNewUrlParser: true});
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 // Creating a schema for the books database
 const bookSchema = new mongoose.Schema({
@@ -474,7 +480,8 @@ app.get(
 
 // ========== Listening for requests ==========
 let port = process.env.PORT;
-
-app.listen(port || 3000, () => {
-  console.log("Server has successfully started.");
-});
+connectDB().then(() => {
+  app.listen(port, () => {
+      console.log("listening for requests");
+  })
+})
