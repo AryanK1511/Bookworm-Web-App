242dc104907f4b5800a1bdd8c1d9f63f4038f3b7
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-23 22:27:09 -0500

    Created sign up page UI validation component

--- bkwrm_backend/app/models/user.py
+++ bkwrm_backend/app/models/user.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # ========== USER MODEL ==========
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)  
