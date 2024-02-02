783aa8a8e4daba2337d52828092a40e0431d3f27
AryanK1511 <aryankhurana1511@gmail.com>
2024-02-02 04:49:33 -0500

    Cleaned up backend code

--- bkwrm_backend/app/__init__.py
+++ bkwrm_backend/app/__init__.py
@@ -3,9 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_cors import CORS
 
+# Create a Flask app
 app = Flask(__name__)
-CORS(app, supports_credentials=True)  # This will enable CORS for all routes and domains
+
+# This will enable CORS for all routes and domains
+CORS(app, supports_credentials=True) 
+
+# Load the config from the config.py file
 app.config.from_object(Config)
+
+# Create a SQLAlchemy database
 db = SQLAlchemy(app)
 
+# Import the routes
 from app.routes import user_routes
--- bkwrm_backend/run.py
+++ bkwrm_backend/run.py
@@ -1,10 +1,14 @@
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
 load_dotenv()
 
 from app import app, db
 
+# Create all tables
 with app.app_context():
     db.create_all()
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
