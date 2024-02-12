124fb0ed9b943e6d6c2a78d750230c48f3ec38bd
AryanK1511 <aryankhurana1511@gmail.com>
2024-02-12 06:08:15 -0500

    Added feature of adding to reading list from the book page itself

--- bkwrm_frontend/lib/readingList.js
+++ bkwrm_frontend/lib/readingList.js
@@ -22,7 +22,7 @@ const addBookToReadingList = async (book) => {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
+				"Authorization": `Bearer ${token}`,
 			},
 			body: JSON.stringify({
 				google_books_id: book.id,
@@ -54,7 +54,7 @@ const getReadingList = async () => {
 		{
 			method: "GET",
 			headers: {
-				Authorization: `Bearer ${token}`,
+				"Authorization": `Bearer ${token}`,
 			},
 		},
 	);
@@ -85,7 +85,7 @@ const deleteBookFromReadingList = async (bookId) => {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
+				"Authorization": `Bearer ${token}`,
 			},
 			body: JSON.stringify({ google_books_id: bookId }),
 		},
@@ -102,6 +102,34 @@ const deleteBookFromReadingList = async (bookId) => {
 	}
 };
 
+// => Check if a book is in the user's reading list
+const checkBookInReadingList = async (googleBooksId) => {
+    const token = getToken();
+
+    // Make a GET request to a new endpoint that checks for book presence in the reading list
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/books/check?google_books_id=${googleBooksId}`,
+        {
+            method: "GET",
+            headers: {
+				"Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            }
+        },
+    );
+
+    const data = await response.json();
+
+    // If the response is ok, return the data indicating whether the book is in the reading list
+    if (response.ok) {
+        return { success: true, inReadingList: data.inReadingList };
+    } else {
+        console.error("Failed to check book in reading list:", data.message);
+        return { success: false, message: data.message };
+    }
+};
+
+
 // => Update book status
 const updateBookStatus = async (bookId, status) => {
 	const token = getToken();
@@ -113,7 +141,7 @@ const updateBookStatus = async (bookId, status) => {
 			method: "PUT",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
+				"Authorization": `Bearer ${token}`,
 			},
 			body: JSON.stringify({ status: status, google_books_id: bookId }),
 		},
@@ -141,7 +169,7 @@ const deleteAllBooks = async () => {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
+				"Authorization": `Bearer ${token}`,
 			},
 		},
 	);
@@ -166,4 +194,5 @@ export {
 	deleteBookFromReadingList,
 	updateBookStatus,
 	deleteAllBooks,
+	checkBookInReadingList
 };
